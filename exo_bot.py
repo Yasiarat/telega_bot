2b4e1aabec412ab004158ff2d79ac49222095b31
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-02-08 12:15:59 +0300

    Add files via upload

--- exo_bot.py
+++ exo_bot.py
@@ -5,8 +5,8 @@ from key import TOKEN
 
 def main():
     updater = Updater(
-        token = TOKEN,
-        use_context = True
+        token=TOKEN,
+        use_context=True
     )
     # диспечер распределяет сообщения по обработчикам
     dispatcher = updater.dispatcher
@@ -15,7 +15,9 @@ def main():
     echo_handler = MessageHandler(Filters.all, do_echo)
     hello_handler = MessageHandler(Filters.text('Привет'), say_hello)
 
-
+    '''hello_handler = MessageHandler(if 'привет' in text.lower():
+        say_hello)
+    '''
     # регестрируем обработчик
     dispatcher.add_handler(hello_handler)
     dispatcher.add_handler(echo_handler)
@@ -25,24 +27,29 @@ def main():
     updater.idle()
 
 
-def do_echo(update: Update, context: CallbackContext):
-    name = update.message.from_user.first_name
+def do_echo(update: Update, context: CallbackContext) -> None:
     id = update.message.chat_id
+    user = update.message.from_user.username
     text = update.message.text
+    sticker = update.message.sticker
+    if sticker:
+        sticker_id = sticker.file_id
+        update.message.reply_sticker(sticker_id)
     update.message.reply_text(text=
-                                f' держи своё {text}\n'
-                                f'а это твой айдишник {id}'
-                              )
+                              f' держи своё {text}\n'
+                              f'а это твой айдишник {id}\n'
+                              f'и вообще ты @{user}\n'
 
+                              )
 
 
 def say_hello(update: Update, context: CallbackContext):
     name = update.message.from_user.first_name
-    id = update.message.chat_id
-    text = update.message.text
     update.message.reply_text(text=f'Привет, {name} \n'
                                 f' приятно познакомиться с живым человеком\n'
                                 f'Я - бот'
                               )
+
+
 if __name__ == '__main__':
     main()

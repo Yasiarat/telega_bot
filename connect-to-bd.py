a5168eb7476ce6f48dcf7b5f2109e83c145a404c
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-02-15 12:15:03 +0300

    Add files via upload

---
+++ connect-to-bd.py
@@ -0,0 +1,11 @@
+from openpyxl import load_workbook
+
+bd = load_workbook('EXEL.xlsx')
+for sheet in bd:
+    print(sheet.title)
+stickers_page = bd['stickers']
+for row in range(1, stickers_page.max_row + 1):
+    for column in range(1, stickers_page.max_column + 1):
+        if stickers_page.cell(row=row, column=column).value == 'привет':
+            print(stickers_page.cell(row=row, column=column + 1).value)
+

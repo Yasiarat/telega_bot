5e12ca0269a13f888eb40ff4957425d4a1257fc1
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-02-21 00:20:22 +0300

    Add files via upload

--- connect-to-bd.py
+++ connect-to-bd.py
@@ -8,4 +8,12 @@ for row in range(1, stickers_page.max_row + 1):
     for column in range(1, stickers_page.max_column + 1):
         if stickers_page.cell(row=row, column=column).value == 'привет':
             print(stickers_page.cell(row=row, column=column + 1).value)
+        if stickers_page.cell(row=row, column=column).value == 'пока':
+            print(stickers_page.cell(row=row, column=column + 1).value)
+        if stickers_page.cell(row=row, column=column).value == 'как дела?':
+            print(stickers_page.cell(row=row, column=column + 1).value)
+        if stickers_page.cell(row=row, column=column).value == 'удачи':
+            print(stickers_page.cell(row=row, column=column + 1).value)
+        if stickers_page.cell(row=row, column=column).value == 'знаешь?':
+            print(stickers_page.cell(row=row, column=column + 1).value)
 

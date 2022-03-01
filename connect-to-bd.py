dd1fdbb5ff7165f9d858b83bbf4a71edc3fc6d3c
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-03-01 12:14:06 +0300

    Add files via upload

--- connect-to-bd.py
+++ connect-to-bd.py
@@ -4,16 +4,28 @@ bd = load_workbook('EXEL.xlsx')
 for sheet in bd:
     print(sheet.title)
 stickers_page = bd['stickers']
+
+
+stickers = {}
+
+
 for row in range(1, stickers_page.max_row + 1):
-    for column in range(1, stickers_page.max_column + 1):
-        if stickers_page.cell(row=row, column=column).value == 'привет':
+    keyword = stickers_page.cell(row=row, column=1).value
+    sticker_id = stickers_page.cell(row=row, column=2).value
+    print(keyword, sticker_id)
+    stickers[keyword] = sticker_id
+
+print(stickers)
+
+'''
+
+     if stickers_page.cell(row=row, column=column).value == 'пока':
             print(stickers_page.cell(row=row, column=column + 1).value)
-        if stickers_page.cell(row=row, column=column).value == 'пока':
+        elif stickers_page.cell(row=row, column=column).value == 'как дела?':
             print(stickers_page.cell(row=row, column=column + 1).value)
-        if stickers_page.cell(row=row, column=column).value == 'как дела?':
+        elif stickers_page.cell(row=row, column=column).value == 'удачи':
             print(stickers_page.cell(row=row, column=column + 1).value)
-        if stickers_page.cell(row=row, column=column).value == 'удачи':
+        elif stickers_page.cell(row=row, column=column).value == 'знаешь?':
             print(stickers_page.cell(row=row, column=column + 1).value)
-        if stickers_page.cell(row=row, column=column).value == 'знаешь?':
-            print(stickers_page.cell(row=row, column=column + 1).value)
+            '''
 

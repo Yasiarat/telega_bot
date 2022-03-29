1a13c5fb62fdc1fb722c2ca1b7f797e14c6adca8
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2022-03-29 12:18:26 +0300

    Add files via upload

--- exo_bot.py
+++ exo_bot.py
@@ -3,6 +3,10 @@ from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from key import TOKEN
 from connect_to_bd import stickers, replies, insert_sticker, in_database, insert_user
 
+grades = [
+    ['8', '9', '10', '11'],
+]
+
 
 def main():
     updater = Updater(
@@ -14,18 +18,16 @@ def main():
 
     # создаём обработчик
     echo_handler = MessageHandler(Filters.all, do_echo)
-    new_sticker_handler = MessageHandler(Filters.text('Добавь, добавь'), new_sticker)
-    text_handler = MessageHandler(Filters.text, say_smth)
+    new_sticker_handler = MessageHandler(Filters.text('Добавить стикер'), new_sticker)
+    text_handler = MessageHandler(Filters.text, ask_grade)
     hello_handler = MessageHandler(Filters.text('Привет'), say_hello)
     bye_handler = MessageHandler(Filters.text('пока'), say_bye)
     keyboard_handler = MessageHandler(Filters.text('Клавиатура, клавиатура'), keyboard)
-    meet_handler = MessageHandler(Filters.text('давай знакомиться'), meet)
 
     # регестрируем обработчик
-    dispatcher.add_handler(meet_handler)
+    dispatcher.add_handler(text_handler)
     dispatcher.add_handler(hello_handler)
     dispatcher.add_handler(keyboard_handler)
-    dispatcher.add_handler(text_handler)
     dispatcher.add_handler(new_sticker_handler)
     dispatcher.add_handler(bye_handler)
     dispatcher.add_handler(echo_handler)
@@ -53,8 +55,8 @@ def do_echo(update: Update, context: CallbackContext) -> None:
 
 def say_hello(update: Update, context: CallbackContext):
     name = update.message.from_user.first_name
-    update.message.reply_text(text=f'Привет, {name}, \n'
-                                f'приятно познакомиться с живым человеком\n'
+    update.message.reply_text(text=f'Привет, {name} \n'
+                                f' приятно познакомиться с живым человеком\n'
                                 f'Я - бот'
                               )
 
@@ -136,51 +138,61 @@ def meet(update: Update, context: CallbackContext):
 def ask_name(update: Update, context: CallbackContext, user_id):
     '''
     спрашиваем имя
-    если имя состоит из букв, двигаемся дальше
+    TODO проверить имя пользователя в телеграме
     '''
     update.message.reply_text(
         'Привет, меня зовут Бот\n' 
         'А тебя?'
     )
-    name = update.message.text
-    if name.isalpha():
-        ask_sex(update, context, name)
+    ask_sex(update, context)
 
 
-def ask_sex(update: Update, context: CallbackContext, user_id, name):
+def ask_sex(update: Update, context: CallbackContext):
     '''
     спрашиваем пол, выводим клавиатуру
     '''
+    name = update.message.text
+    if not name.isalpha():
+        ask_name(update, context)
+    context.user_data['name'] = name
     buttons = [
         ['м', 'ж'],
     ]
-    update.message.reply_text(
-        text=f'{name}, укажи пожалуйста свой пол',
-        reply_markup=ReplyKeyboardMarkup(
-            buttons,
-            resize_keyboard=True
-        )
+    keys = ReplyKeyboardMarkup(
+        buttons,
+        resize_keyboard=True#размер
     )
-    sex = update.message.text
+    update.message.reply_text(
+        text=f'Приятно познакомиться, {name}, укажи пожалуйста свой пол',
+        reply_markup=keys# разметка
+    )
 
-    ask_grade(update, context, name, sex)
 
-
-def ask_grade(update: Update, context: CallbackContext, user_id, name, sex):
+def ask_grade(update: Update, context: CallbackContext):
     '''
     спрашиваем класс с помощью клавиатуры
     '''
-    buttons = [
-        ['8', '9', '10', '11'],
-    ]
+    sex = update.message.text
+    if sex != 'м' and sex != 'ж':
+        ask_sex(update, context)
+    context.user_data['sex'] = sex
+    keys = ReplyKeyboardMarkup(
+        grades,
+        resize_keyboard=True  # размер
+    )
     update.message.reply_text(
         text='Укажи пожалуйста свой класс',
-        reply_markup=ReplyKeyboardMarkup(
-            buttons,
-            resize_keyboard=True
-        )
+        reply_markup=keys
     )
+
+
+def greet(update: Update, context: CallbackContext):
     grade = update.message.text
+    if grade in grades:
+        ask_grade(update, context)
+    name = context.user_data['name']
+    sex = context.user_data['sex']
+    user_id = update.message.from_user.id
     insert_user(user_id, name, sex, grade)
 
 
